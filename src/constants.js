a785b29f226057595e829bf4457b82d7cefff268
Elyes MESSAADIA <168727602+elyes-messaadia@users.noreply.github.com>
2024-08-23 10:55:41 +0200

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `C'est mon PC, celui avec lequel je vous fais une présentation, j'adore les PC portables. Je passe une grande partie de mon temps dessus à jouer et apprendre de nouvelles choses en programmation. Voici le lien de mon <a href="https://github.com/elyes-messaadia" target="_blank">Github</a>`,
+  resume:`Voici le CV sous format Web que je n'ai toujours pas terminé de faire...`,
+
+  "sofa-table": `C'est mon canapé, j'adore me poser dessus et regarder des vidéos sur YouTube. `,
+  
+  tv: `C'est ma télé. J'ai regardé beaucoup de vidéos sur le thème de la programmation récemment :
+   <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (beaucoup, incroyable PirateSoftware je recommande ! `,
+
+  bed: `C'est ici que je dors, j'adore dormir et ça fait du bien d'aller se reposer dessus.`,
+ 
+  library: `C'est ma bibliothèque, j'adore lire des livres sur la philosophie (Épictète, Aristote, Lao Tzu, Sénèque...)`,
+  
+  exit: `Si tu veux quitter mon portfolio, ferme simplement la page.`,
+};
