584f3982d39ca1d784e3ffa6a02d8a1639a64487
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2025-07-08 20:51:05 +0200

    Updates

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-
-export default defineConfig({
-    base: "/2D-Portfolio/", // ← important pour GitHub Pages
-    build: {
-        minify: "terser",
-    },
-});
