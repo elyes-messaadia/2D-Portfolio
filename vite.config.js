94771f76d1f556d7fd5f030f0dfd9119c0c98091
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2025-07-09 11:53:42 +0200

    Supprimer les fichiers index.html et configurer Vite pour retirer les options de construction obsolètes, tout en ajoutant la page d'accueil dans package.json.

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
   base: '/2D-Portfolio/',
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.html'), // ✅ correction ici
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
