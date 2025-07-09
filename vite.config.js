0c0cf978aacc8f5c0014671db612250096cd6657
Elyes MESSAADIA <elyes.messaadia@laplateforme.io>
2025-07-09 11:18:49 +0200

    Supprimer les fichiers index.html et kaboomCtx.js, et mettre à jour la configuration de Vite pour retirer les options de construction obsolètes.

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import path from 'path';
 
 export default defineConfig({
   base: '/2D-Portfolio/',
-  build: {
-    rollupOptions: {
-      input: path.resolve(__dirname, 'src/index.html'), // ✅ correction ici
-    },
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
